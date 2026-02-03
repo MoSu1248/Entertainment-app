fd095c6517d3f885d4353288e9891abe5768f38c
MoSu1248 <mohammedsuhailr2@gmail.com>
2026-02-03 12:58:37 +0200

    fixed overlay

--- src/components/Overlay/Popup.jsx
+++ src/components/Overlay/Popup.jsx
@@ -5,15 +5,22 @@ import { useOverlayStore } from "../Store/useOverlayStore";
 
 export default function Popup() {
   const logout = useLoginStore((state) => state.logout);
-  const hideLogout = useOverlayStore((state)=> state.hideLogout);
+  const hideLogout = useOverlayStore((state) => state.hideLogout);
+
+  function handleLogout() {
+    logout();
+    hideLogout();
+  }
 
   return (
     <div className="popup__wrapper">
       <div className="popup__container">
         <h2>Logout?</h2>
         <div className="button__container">
-          <button onClick={logout}>Confirm</button>
-          <button className="cancel" onClick={hideLogout}>Cancel</button>
+          <button onClick={() => handleLogout()}>Confirm</button>
+          <button className="cancel" onClick={hideLogout}>
+            Cancel
+          </button>
         </div>
       </div>
     </div>
